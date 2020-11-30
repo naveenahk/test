ee922c3de760b6ed4f6b3ed75fd97bb3d4e6a890
NaveenaHK <naveenahk11@gmail.com>
2020-11-30 18:09:56 +0530

    Updated to consider correct zip file

--- builder.sh
+++ builder.sh
@@ -16,14 +16,14 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
    && apt-get update \
    && apt-get install -y yarn
    
-wget https://github.com/fcwu/docker-ubuntu-vnc-desktop/archive/focal-lxqt.zip
-unzip docker-ubuntu-vnc-desktop-focal-lxqt.zip
+wget https://github.com/fcwu/docker-ubuntu-vnc-desktop/archive/develop.zip
+unzip develop.zip
 
 mkdir -p /src/web
-cp -R docker-ubuntu-vnc-desktop-focal-lxqt/web/* /src/web
+cp -R docker-ubuntu-vnc-desktop-develop/web/* /src/web
 cd /src/web \
    && yarn config set network-timeout 600000 -g \
-   && yarn install --verbose
+   && yarn install --verbose \
    && yarn run build
    
 sed -i 's#app/locale/#novnc/app/locale/#' /src/web/dist/static/novnc/app/ui.js
